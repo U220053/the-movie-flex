065b9bcd7fe68434236e4e2ce09c9d5e7ad3ef05
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-01-04 22:32:54 +0530

    star rating push

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Card.css";
 import CardDemo from "../../CardDemo.webp"
 function Card({ details }) {
+    const id = 3.5
   return (
     <>
     <div className="card">
@@ -14,6 +15,13 @@ function Card({ details }) {
                     <h2 className="cardHeading">Money Hiest</h2>
                     <h5 className="cardSubheading">- The korean</h5>
                     <p className="cardPara">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam unde dicta ut eaque tenetur facilis.</p>
+                    <div className="cardRating">
+                        <i class={(id>=1)?"fa-solid fa-star":"fa-regular fa-star"}></i>
+                        <i class={(id>=2)?"fa-solid fa-star":"fa-regular fa-star"}></i>
+                        <i class={(id>=3)?"fa-solid fa-star":"fa-regular fa-star"}></i>
+                        <i class={(id>=4)?"fa-solid fa-star":"fa-regular fa-star"}></i>
+                        <i class={(id>=5)?"fa-solid fa-star":"fa-regular fa-star"}></i>
+                    </div>
                 </div>
             </div>
         </div>
